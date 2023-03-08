508db8b8a6fdcfa1405c047fac9481ca7d158a4f
Ioannis <powerfulguy4774@gmail.com>
2023-03-08 17:44:26 +0100

    Making items removable

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 
-import Modal from '../../components/UI/Modal';
+import Modal from '../UI/Modal';
 import CartItem from './CartItem';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
@@ -11,9 +11,13 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemAddHandler = (item) => {};
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes['cart-items']}>
